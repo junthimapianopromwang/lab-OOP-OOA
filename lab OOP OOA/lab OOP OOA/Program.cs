d85d42d435c9fb7f322868b4535a0aeadcc71ee3
junthimapianopromwang <junthima.p@kkumail.com>
2022-11-28 15:45:29 +0700

    name

---
+++ lab OOP OOA/lab OOP OOA/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//name
+Console.WriteLine("junthiam promwang");
+// id
+Console.WriteLine(653450280-1);
+// email
+Console.WriteLine("junthima.p@kkumail.com");
